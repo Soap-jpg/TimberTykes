f0e6428bad59c70512fc3ba4569f7152742c061e
Soap-jpg <mason.alex2102@gmail.com>
2025-04-06 04:26:08 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const path = require("path");
+
+const app = express();
+
+const expRoutes = require("./routes/exp");
+const main_page = require("./routes/main_page");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(expRoutes);
+app.use(main_page);
+
+app.listen(3000);
